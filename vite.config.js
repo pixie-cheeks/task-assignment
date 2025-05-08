113459a1d4b3609b020057e30b5bd4daceb586e2
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-08 18:53:24 +0530

    Finish proper build settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: 'src',
-  build: { outDir: '../dist', emptyOutDir: true },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(dirname, 'src/index.html'),
+        nested: path.resolve(dirname, 'src/contact-page/index.html'),
+      },
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 });
